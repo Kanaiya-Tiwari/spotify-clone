9b2e8aa6c967e75de5d176a6d0d977965d52d4e1
Kanaiya-Tiwari <94900554+Kanaiya-Tiwari@users.noreply.github.com>
2022-04-17 20:26:07 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+// const nextConfig = {
+//   reactStrictMode: true,
+//   images: {
+//     domains: ['rb.gy'],
+//   },
+// }
+
+// module.exports = nextConfig
+module.exports = {
   images: {
     domains: ['rb.gy'],
   },
 }
-
-module.exports = nextConfig
